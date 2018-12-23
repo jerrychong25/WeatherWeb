5329128d4b9a92f262ff3598d5194d1614ee43a5
Jerry Chong <15714095+jerrychong25@users.noreply.github.com>
2018-12-23 22:32:44 +0800

    Added service worker

--- scripts/app.js
+++ scripts/app.js
@@ -199,4 +199,12 @@ var injectedForecast = {
     });
   });
 
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+     .register('/service-worker.js')
+     .then(function() { 
+        console.log('Service Worker Registered'); 
+      });
+  }
+
 })();
